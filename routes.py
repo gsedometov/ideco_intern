b8570b5f1b84543a8bee1fcc14061d0e7c3f6488
gsedometov <gsedometov@gs-deb>
2017-03-18 20:08:05 +0500

    Made routing async

--- routes.py
+++ routes.py
@@ -2,18 +2,18 @@ from aiohttp import web
 
 from service import daemon
 
-def start(request):
+async def start(request):
     daemon.run()
     return web.Response(text='Сервис запущен')
 
-def restart(request):
+async def restart(request):
     daemon.restart()
     return web.Response(text='Сервис перезапущен')
 
-def stop(request):
+async def stop(request):
     daemon.stop()
     return web.Response(text='Сервис остановлен')
 
-def switch(request):
+async def switch(request):
     daemon.switch()
     return web.Response(text='Сервис переключён')
